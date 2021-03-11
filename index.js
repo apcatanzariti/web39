e2be01f820e8a57e97ba025863c93c7de9abda26
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-11 13:02:56 -0500

    test

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env varia
     server.use(cors());
 }
 
+server.use('*', (req, res) => {
+    res.send('<h1>success</h1>');
+});
+
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
