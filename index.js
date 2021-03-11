dc4b25776cb57c0bc32134d96df2d06dccef7b68
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-11 12:40:43 -0500

    test commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+console.log('web39 rules!');
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env variable is called "NODE_ENV" --> "production"
+    const cors = require('cors');
+    server.use(cors());
+}
