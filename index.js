570cab4270331f9a284d297716f3bc12d9768bdd
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-11 12:48:26 -0500

    server listening correctly

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-console.log('web39 rules!');
-
 const express = require('express');
 
 const server = express();
@@ -12,3 +10,9 @@ if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env varia
     const cors = require('cors');
     server.use(cors());
 }
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
